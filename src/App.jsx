51083ef3aa2af5be36b50fe12aa45c2a9fcb5d09
AlonVizniuk <128905873+AlonVizniuk@users.noreply.github.com>
2024-04-29 16:44:39 +0300

    location and double date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import './App.css'
+import {Datetoggle} from "./Components/Datetoggle"
+import { Locationinput } from './Components/Locationinput'
+
 
 function App() {
 
   return (
     <>
+      <Datetoggle/>
+      <Locationinput/>
     </>
   )
 }
---
+++ src/Components/Datetoggle.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import Box from '@mui/material/Box';
+
+export const Datetoggle = () => {
+  const [open, setOpen] = React.useState(false);
+  const [startDate, setStartDate] = React.useState(null);
+  const [endDate, setEndDate] = React.useState(null);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleApply = () => {
+    // Handle the selected start and end dates here
+    console.log('Start Date:', startDate);
+    console.log('End Date:', endDate);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open Date Range Picker</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Select Date Range</DialogTitle>
+        <DialogContent>
+          <Box>
+            <InputLabel htmlFor="start-date">Start Date</InputLabel>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                id="start-date"
+                value={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </LocalizationProvider>
+          </Box>
+          <Box mt={2}>
+            <InputLabel htmlFor="end-date">End Date</InputLabel>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                id="end-date"
+                value={endDate}
+                onChange={(date) => setEndDate(date)}
+              />
+            </LocalizationProvider>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleApply}>Apply</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
