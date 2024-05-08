9c4d641d136408126ba40a8e784c6ea34fdecd45
inonELGABSI <inon11221@gmail.com>
2024-05-08 22:55:30 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import { Generalsearch } from './Components/Generalsearch'
+import { Generalsearch } from './components/Generalsearch'
 // import { Navbar } from './Components/Navbar'
-import { Itemscontainer } from './Components/Itemscontainer'
+import { Itemscontainer } from './components/Itemscontainer'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { blue } from '@mui/material/colors';
-import { Navimg } from './Components/Navimg';
-import { Cardgrid } from './Components/Cardgrid';
+import { Navimg } from './components/Navimg';
+import { Cardgrid } from './components/Cardgrid';
 import Box from '@mui/material/Box';
 import { Container } from '@mui/system';
-import LoginSignupPopup from './Components/LoginSignupPopup';
+import LoginSignupPopup from './components/LoginSignupPopup';
 
 const theme = createTheme({
     palette: {
@@ -25,10 +25,9 @@ function App() {
   const src ="src/images/navimg.jpg";
   return (
     <>
-      <Generalsearch/>
-     <ThemeProvider theme={theme}>
-
-     {/*<Navbar />*/}
+    <Generalsearch/>
+    <ThemeProvider theme={theme}>
+     <Navbar />
      <Navimg src={src} />
      
      <Box sx={{ display: 'flex',flexDirection: 'column', justifyContent: 'center',marginTop:'20px'}}>
